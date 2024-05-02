612e01b152dce688ea3dffb61dfb1192c8757e56
Giovanni Tran <giovanni.s.tran@sjsu.edu>
2024-05-01 22:15:22 -0700

    Update Login/Register/Intro formatting

--- client/src/pages/Intro.jsx
+++ client/src/pages/Intro.jsx
@@ -1,8 +1,8 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Intro = () => {
   return (
-    <div className='-full py-12 md:py-24 lg:py-32 xl:py-48 flex items-center justify-center'>
+    <div className="-full py-12 md:py-24 lg:py-32 xl:py-48 flex items-center justify-center">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center space-y-4 text-center">
           <div className="space-y-2">
@@ -10,7 +10,9 @@ const Intro = () => {
               Reach Your Fitness Goals
             </h1>
             <p className="mx-auto max-w-[700px] text-gray-500 md:text-x1">
-              Streamline your exercise schedule with our user-oriented workout manager. Create workout routines, use community-curated workouts, or share your own!
+              Streamline your exercise schedule with our user-oriented workout
+              manager. Create workout routines, use community-curated workouts,
+              or share your own!
             </p>
           </div>
           <div className="space-x-4">
@@ -30,8 +32,7 @@ const Intro = () => {
         </div>
       </div>
     </div>
-    
   );
-}
+};
 
 export default Intro;
